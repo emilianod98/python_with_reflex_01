83e6c6f86d0286a7df20a568ed0c4a458394c8b9
emilianod98 <emidaniele98@gmail.com>
2024-04-26 07:33:18 -0300

    python_with_reflex_01

---
+++ proyect_with_reflex/proyect_with_reflex_counter.py
@@ -0,0 +1,31 @@
+from rxconfig import config
+import reflex as rx
+
+class State(rx.State):
+    count: int = 0
+    
+    def increment(self):
+        self.count += 1
+        
+    def decrement(self):
+        self.count -= 1
+
+def index():
+    return rx.hstack(
+        rx.button(
+            "Decrement",
+            color_scheme="red",
+            border_radius="1em",
+            on_click=State.decrement,
+        ),
+        rx.heading(State.count, font_size="2em"),
+        rx.button(
+            "Increment",
+            color_scheme="green",
+            border_radius="1em",
+            on_click=State.increment,
+        ),
+    )
+
+app = rx.App()
+app.add_page(index)

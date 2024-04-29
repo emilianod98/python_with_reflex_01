7d8b26041b15948078c371cf0e8ed2e8ef8b64a7
Emiliano Daniele <emidaniele98@gmail.com>
2024-04-28 23:31:08 -0300

    Add files via upload

--- proyect_with_reflex/proyect_with_reflex.py
+++ proyect_with_reflex/proyect_with_reflex.py
@@ -2,38 +2,28 @@
 import reflex as rx
 
 from chatapp import style
+from chatapp.state import TutorialState
 
 
 def qa(question: str, answer: str) -> rx.Component:
     return rx.box(
         rx.box(
-            rx.text(question, style=style.question_style),
-            text_align="right",
+            rx.text(question, text_align="right"),
+            style=style.question_style,
         ),
         rx.box(
-            rx.text(answer, style=style.answer_style),
-            text_align="left",
+            rx.text(answer, text_align="left"),
+            style=style.answer_style,
         ),
-        margin_y="1em",
     )
 
 
 def chat() -> rx.Component:
-    qa_pairs = [
-        (
-            "What is Reflex?",
-            "A way to build web apps in pure Python!",
-        ),
-        (
-            "What can I make with it?",
-            "Anything from a simple website to a complex web app!",
-        ),
-    ]
     return rx.box(
-        *[
-            qa(question, answer)
-            for question, answer in qa_pairs
-        ]
+        rx.foreach(
+            TutorialState.chat_history,
+            lambda messages: qa(messages[0], messages[1]),
+        )
     )
 
 
@@ -41,9 +31,15 @@ def action_bar() -> rx.Component:
     return rx.hstack(
         rx.input(
             placeholder="Ask a question",
+            value=TutorialState.question,
+            on_change=TutorialState.set_question,
             style=style.input_style,
         ),
-        rx.button("Ask", style=style.button_style),
+        rx.button(
+            "Ask",
+            on_click=TutorialState.answer,
+            style=style.button_style,
+        ),
     )
 
 
